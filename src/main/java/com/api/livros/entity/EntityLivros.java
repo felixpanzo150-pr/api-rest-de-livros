42bd23f97baa071ed0756ab7b96f383c1da8e23d
felixpanzo150-pr <felixpanzo150@gmail.com>
2025-09-02 13:03:18 +0100

    feat: Criando a classe de entidades.

--- src/main/java/com/api/livros/entity/EntityLivros.java
+++ src/main/java/com/api/livros/entity/EntityLivros.java
@@ -1,3 +1,82 @@
 package com.api.livros.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class EntityLivros {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long Id;
+
+    private String nome;
+    private String autor;
+    private int ano;
+    private double preco;
+
+    public EntityLivros(Long id, String autor, String nome, int ano, double preco) {
+        Id = id;
+        this.autor = autor;
+        this.nome = nome;
+        this.ano = ano;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public EntityLivros(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "EntityLivros{" +
+                "Id=" + Id +
+                ", nome='" + nome + '\'' +
+                ", autor='" + autor + '\'' +
+                ", ano=" + ano +
+                ", preco=" + preco +
+                '}';
+    }
+}
